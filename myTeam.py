1c908f03934cf84da0b0ba250be2b5fab541c3b6
JMGutie80 <151789907+JMGutie80@users.noreply.github.com>
2023-11-28 20:30:22 -0800

    Update myTeam.py
    Mario test2

--- myTeam.py
+++ myTeam.py
@@ -8,7 +8,7 @@ from pacai.agents.capture.reflex import CaptureAgent
 # TODO: do not import qulafied import THIS IS ONE OF THE CHECKS KAIA
 # IDFK WHAT TO DO I WANNA CRYYYYYY
 # TODO: make both agents offensive agents instead of defenisve offensive??
-
+# Mario test
 def createTeam(firstIndex, secondIndex, isRed,
         first = 'pacai.agents.capture.dummy.DummyAgent',
         second = 'pacai.agents.capture.dummy.DummyAgent'):
